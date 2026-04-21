71e9f93ac2844c166a5a05fd62e78e5cb2db8c30
ayebaishmo <ishmo255super@gmail.com>
2026-04-21 10:14:13 +0300

    Add a blueprint to the app functionality

--- ai_backend/app.py
+++ ai_backend/app.py
@@ -4,6 +4,7 @@ from routes.admin_route import admin_bp
 from routes.user_route import user_bp
 from routes.user_show import usershow_bp
 from routes.apikey import api_key_create_bp
+from routes.auth_chat_route import chat_secure_bp
 from flasgger import Swagger
 import os
 from config.services.extensions import db
@@ -27,6 +28,7 @@ app.register_blueprint(admin_bp, url_prefix="/api/v1")
 app.register_blueprint(user_bp, url_prefix="/api/v1")
 app.register_blueprint(usershow_bp, url_prefix="/api/v1")
 app.register_blueprint(api_key_create_bp, url_prefix="/api/v1")
+app.register_blueprint(chat_secure_bp, url_prefix="/api/v1")
 
 with app.app_context():
     db.create_all()
