4498a0a2bccd5d24f799e21c5f02df1c9f6c7ad4
ayebaishmo <ishmo255super@gmail.com>
2026-04-21 10:12:19 +0300

    Add auth route

---
+++ ai_backend/routes/auth_chat_route.py
@@ -0,0 +1,78 @@
+from flask import Blueprint, request, jsonify
+from models.apikey import ApiKey
+from config.services.ollama_services import generate_responce
+
+chat_secure_bp = Blueprint("chat_secure", __name__)
+
+@chat_secure_bp.route("/chat/secure", methods=["POST"])
+def chat_secure():
+    """
+    Autheticated Chat with ai
+    ---
+    tags:
+        - Chat:
+    parameters:
+        - name: x-api-key
+          in: header
+          required: true
+          type: string
+          description: User API key
+        - name: body
+          in: body
+          required: true
+          schema:
+            type: object
+            required:
+               - messages
+            properties:
+              model:
+                type: string
+                example: gemma 4
+              messages:
+                type: array
+                itmes:
+                type: object
+                properties:
+                   role:
+                      type: string
+                      example: user
+                   content:
+                      type: string
+                      example: Hello AI
+    responses:
+        200:
+          description: AI response
+        401:
+          description: Unauthorized
+    """
+
+    api_key_value = request.headers.get("x_api_key")
+     
+    if not api_key_value:
+        return jsonify({"error": "API key required"}), 401
+    
+    api_key = ApiKey.query.filter_by(
+        key = api_key_value,
+        is_active=True
+    ). first()
+
+    if not api_key:
+        return jsonify({"error": "Invalid API key"}), 401
+    
+    user = api_key.user
+
+    data = request.get_json() or {}
+
+    messages = data.get("message", [])
+    model = data.get("model", "gemma4")
+
+    if not messages:
+        return jsonify({"error": "Messages are required"}), 400
+    
+    response = generate_responce(messages, model)
+
+    return jsonify({
+        "response": response,
+        "user_id": user.id
+    }), 200
+    
